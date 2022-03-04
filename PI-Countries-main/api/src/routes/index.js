6389d3518f17698c0d0baadabdf39e6b687a71f7
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-03-04 17:08:24 -0300

    index modified

--- PI-Countries-main/api/src/routes/index.js
+++ PI-Countries-main/api/src/routes/index.js
@@ -2,16 +2,16 @@ const { Router } = require('express');
 const {
     getCountries,
     getCountriesById
-} = require('../routes/ctrls/country')
+} = require('./ctrls/country')
 const {
     getActivitiesByName,
     postActivity
-} = require('../routes/ctrls/activity')
+} = require('./ctrls/activity')
 
 const router = Router();
 const {Country, Activity, country_activity} = require('../db')
 
-router.get("/countries", getCountries)
+router.get('/countries', getCountries)
 router.get("/countries/:id", getCountriesById)
 router.get("/activities", getActivitiesByName)
 router.post("/activity", postActivity)
