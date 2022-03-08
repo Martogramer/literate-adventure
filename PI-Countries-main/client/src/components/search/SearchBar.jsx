cd4a49a5345869eaa8922c88f0d297097b6b4fdc
Martogramer <86386122+Martogramer@users.noreply.github.com>
2022-03-07 22:41:30 -0300

    search comp

---
+++ PI-Countries-main/client/src/components/search/SearchBar.jsx
@@ -0,0 +1,12 @@
+const SearchBar=()=>{
+    return(
+        <header>
+            <nav>
+                <div>
+                    <h2></h2>
+                </div>
+            </nav>
+        </header>
+    )
+}
+export default SearchBar
